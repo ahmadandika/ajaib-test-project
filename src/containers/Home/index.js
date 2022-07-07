2bf06b82e68e2aae59b83fd8a28905209e02649f
ahmadandika <ahmad.andika212@gmail.com>
2022-07-07 22:24:36 +0700

    implement pagination

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReusableTable from 'components/ReusableTable';
 import { getUser } from 'services';
-import { Box } from '@mui/material';
+import { Box, TablePagination } from '@mui/material';
 import Filter from './components/Filter';
 
+const MOCK_TOTAL = 100;
+
 const App = () => {
+  const [page, setPage] = React.useState(1);
+  const [pageSize, setPageSize] = React.useState(5);
   const [rows, setRows] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
 
   const columns = [
     {
@@ -59,24 +64,44 @@ const App = () => {
   ];
 
   const loadData = async () => {
+    setLoading(true);
     try {
-      const res = await getUser({ page: 1, results: 10 });
-      console.log('res', res);
+      const res = await getUser({ page, results: pageSize, pageSize });
       const result = res.data.results;
       setRows(result);
     } catch (error) {
       setRows([]);
+    } finally {
+      setLoading(false);
     }
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setPageSize(parseInt(event.target.value, 10));
+    setPage(0);
   };
 
   React.useEffect(() => {
     loadData();
-  }, []);
+  }, [page, pageSize]);
 
   return (
     <Box>
       <Filter />
-      <ReusableTable rows={rows} columns={columns} loading={false} />
+      <ReusableTable rows={rows} columns={columns} loading={loading} />
+      <TablePagination
+        component="div"
+        count={MOCK_TOTAL}
+        page={page}
+        onPageChange={handleChangePage}
+        rowsPerPage={pageSize}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+        rowsPerPageOptions={[5, 10, 15]}
+      />
     </Box>
   );
 };
