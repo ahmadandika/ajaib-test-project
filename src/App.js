f0c5bbb7cb4827ce2a9793e2f74964e18a153f3d
ahmadandika <ahmad.andika212@gmail.com>
2022-07-08 14:47:28 +0700

    container to pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from 'components/Layout';
-import Home from 'containers/Home';
+import Home from 'pages/Home';
 
 const App = () => {
   return (
